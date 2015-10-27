92412b640b5d4e7447d97313dd07335e78caf087
Jamie Hoover <uipoet@users.noreply.github.com>
2015-10-27 13:53:18 -0700

    Sort order of imports.

--- examples/client/isomorphic.js
+++ examples/client/isomorphic.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {createHistory} from 'history';
-import {render} from 'react-dom';
 import {Router} from 'react-router';
 import StateContext from '../components/state-context';
+import {createHistory} from 'history';
+import {render} from 'react-dom';
 import routes from '../routes';
 
 const {JSON, document} = global;
